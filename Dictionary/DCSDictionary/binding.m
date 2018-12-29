293cc731ce4a56905cb735d37a9e09030d3a9cc0
WindProphet <lihanyuan1996@gmail.com>
2018-12-29 17:35:22 +0900

    DCSRecord Support

--- Dictionary/DCSDictionary/binding.m
+++ Dictionary/DCSDictionary/binding.m
@@ -3,13 +3,14 @@
 #import <Foundation/Foundation.h>
 #import <ruby/ruby.h>
 
-VALUE m_dcs, c_dic;
+VALUE m_dcs, c_dic, c_rec;
 
 #pragma mark header
 
 VALUE dic_from_ref(DCSDictionaryRef ref);
+VALUE rec_from_ref(DCSRecordRef ref);
 
-#pragma mark ruby binding
+#pragma mark binding DCSDictionary
 
 void dic_free(void *data) { free(data); }
 
@@ -195,6 +196,43 @@ VALUE dic_definitions(VALUE self, VALUE text) {
   return cftype2rb(definitions);
 }
 
+VALUE dic_definition_records(VALUE self, VALUE text) {
+  DCSDictionaryRef *dicp;
+  TypedData_Get_Struct(self, DCSDictionaryRef, &dic_type, dicp);
+  NSString *word = [NSString stringWithUTF8String:StringValueCStr(text)];
+  CFArrayRef v = DCSCopyDefinitionRecords(*dicp, (__bridge CFStringRef)word,
+                                          CFRangeMake(0, word.length));
+  VALUE rbarr = rb_ary_new();
+  if (!v)
+    return rbarr;
+  CFIndex c = CFArrayGetCount(v);
+  CFTypeRef list[c];
+  CFArrayGetValues(v, CFRangeMake(0, c), list);
+  for (CFIndex i = 0; i < c; i++) {
+    rb_ary_push(rbarr, rec_from_ref(list[i]));
+  }
+  return rbarr;
+}
+
+VALUE dic_search(VALUE self, VALUE text) {
+  DCSDictionaryRef *dicp;
+  TypedData_Get_Struct(self, DCSDictionaryRef, &dic_type, dicp);
+  NSString *word = [NSString stringWithUTF8String:StringValueCStr(text)];
+  CFArrayRef v = DCSCopyRecordsForSearchString(
+      *dicp, (__bridge CFStringRef)word, CFRangeMake(0, word.length));
+
+  VALUE rbarr = rb_ary_new();
+  if (!v)
+    return rbarr;
+  CFIndex c = CFArrayGetCount(v);
+  CFTypeRef list[c];
+  CFArrayGetValues(v, CFRangeMake(0, c), list);
+  for (CFIndex i = 0; i < c; i++) {
+    rb_ary_push(rbarr, rec_from_ref(list[i]));
+  }
+  return rbarr;
+}
+
 VALUE dic_inspect(VALUE self) {
   VALUE sn = rb_funcall(self, rb_intern("short_name"), 0);
   if (sn == Qnil) {
@@ -221,6 +259,134 @@ VALUE dic_equal(VALUE this, VALUE that) {
   return (*dicp == *dicp2) ? Qtrue : Qfalse;
 }
 
+#pragma mark binding DCSRecord
+
+void rec_free(void *data) { free(data); }
+
+size_t rec_size(const void *data) { return sizeof(DCSRecordRef); }
+
+static const rb_data_type_t rec_type = {
+    .wrap_struct_name = "DCSRecord",
+    .function =
+        {
+            .dmark = NULL,
+            .dfree = rec_free,
+            .dsize = rec_size,
+        },
+    .data = NULL,
+    .flags = RUBY_TYPED_FREE_IMMEDIATELY,
+};
+
+VALUE rec_from_ref(DCSRecordRef ref) {
+  VALUE rec = rb_class_new_instance(0, NULL, c_rec);
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(rec, DCSRecordRef, &rec_type, recp);
+  *recp = ref;
+  return rec;
+}
+
+VALUE rec_alloc(VALUE self) {
+  DCSRecordRef *data = malloc(sizeof(DCSRecordRef));
+  *data = NULL;
+  return TypedData_Wrap_Struct(self, &rec_type, data);
+}
+
+VALUE rec_init(VALUE self) { return self; }
+
+VALUE rec_data(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordCopyData(*recp);
+  return cftype2rb(v);
+}
+
+VALUE rec_data_url(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordCopyDataURL(*recp);
+  return cftype2rb(v);
+}
+
+VALUE rec_definition(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordCopyDefinition(*recp);
+  return cftype2rb(v);
+}
+
+// Error: EXC_BAD_ACCESS
+// VALUE rec_text_elements(VALUE self) {
+//   DCSRecordRef *recp;
+//   TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+//   CFTypeRef v = DCSRecordCopyTextElements(*recp);
+//   return cftype2rb(v);
+// }
+
+VALUE rec_anchor(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordGetAnchor(*recp);
+  return cftype2rb(v);
+}
+
+VALUE rec_associated_obj(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordGetAssociatedObj(*recp);
+  return cftype2rb(v);
+}
+
+VALUE rec_dictionary(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  DCSDictionaryRef v = DCSRecordGetDictionary(*recp);
+  if (!v)
+    return Qnil;
+  return dic_from_ref(v);
+}
+
+VALUE rec_headword(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordGetHeadword(*recp);
+  return cftype2rb(v);
+}
+
+VALUE rec_raw_headword(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordGetRawHeadword(*recp);
+  return cftype2rb(v);
+}
+
+VALUE rec_string(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordGetString(*recp);
+  return cftype2rb(v);
+}
+
+VALUE rec_sub_dictionary(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordGetSubDictionary(*recp);
+  return cftype2rb(v);
+}
+
+VALUE rec_supplemental_headword(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordGetSupplementalHeadword(*recp);
+  return cftype2rb(v);
+}
+
+VALUE rec_title(VALUE self) {
+  DCSRecordRef *recp;
+  TypedData_Get_Struct(self, DCSRecordRef, &rec_type, recp);
+  CFTypeRef v = DCSRecordGetTitle(*recp);
+  return cftype2rb(v);
+}
+
 void Init_binding() {
   m_dcs = rb_define_module("DictionaryServices");
   c_dic = rb_define_class_under(m_dcs, "DCSDictionary", rb_cData);
@@ -241,8 +407,29 @@ void Init_binding() {
   rb_define_method(c_dic, "sub_dictionaries", dic_sub_dictionaries, 0);
   rb_define_method(c_dic, "text_definition", dic_text_definition, 1);
   rb_define_method(c_dic, "definitions", dic_definitions, 1);
+  rb_define_method(c_dic, "definition_records", dic_definition_records, 1);
+  rb_define_method(c_dic, "search", dic_search, 1);
   rb_define_method(c_dic, "inspect", dic_inspect, 0);
   rb_define_method(c_dic, "==", dic_equal, 1);
+
+  c_rec = rb_define_class_under(m_dcs, "DCSRecord", rb_cData);
+  rb_define_alloc_func(c_rec, rec_alloc);
+  rb_define_method(c_rec, "initialize", rec_init, 0);
+  rb_define_method(c_rec, "data", rec_data, 0);
+  rb_define_method(c_rec, "data_url", rec_data_url, 0);
+  rb_define_method(c_rec, "definition", rec_definition, 0);
+  // Error: EXC_BAD_ACCESS
+  // rb_define_method(c_rec, "text_elements", rec_text_elements, 0);
+  rb_define_method(c_rec, "anchor", rec_anchor, 0);
+  rb_define_method(c_rec, "associated_obj", rec_associated_obj, 0);
+  rb_define_method(c_rec, "dictionary", rec_dictionary, 0);
+  rb_define_method(c_rec, "headword", rec_headword, 0);
+  rb_define_method(c_rec, "raw_headword", rec_raw_headword, 0);
+  rb_define_method(c_rec, "string", rec_string, 0);
+  rb_define_method(c_rec, "sub_dictionary", rec_sub_dictionary, 0);
+  rb_define_method(c_rec, "supplemental_headword", rec_supplemental_headword,
+                   0);
+  rb_define_method(c_rec, "title", rec_title, 0);
 
   rb_define_module_function(m_dcs, "getActiveDictionaries",
                             r_getActiveDictionaries, 0);
--- Dictionary/DCSDictionary/lost_define.h
+++ Dictionary/DCSDictionary/lost_define.h
@@ -3,6 +3,8 @@
 #ifndef fissure_dictionary_lost_define_h
 #define fissure_dictionary_lost_define_h
 
+typedef const struct CF_BRIDGED_TYPE(id) __DCSRecord *DCSRecordRef;
+
 CFSetRef DCSGetActiveDictionaries(void);
 CFSetRef DCSCopyAvailableDictionaries(void);
 CFTypeRef DCSDictionaryGetAssetObj(DCSDictionaryRef);
@@ -20,5 +22,21 @@ CFTypeRef DCSDictionaryGetSubDictionaries(DCSDictionaryRef);
 CFTypeID DCSDictionaryGetTypeID(void);
 CFURLRef DCSDictionaryGetURL(DCSDictionaryRef);
 CFArrayRef DCSCopyDefinitions(DCSDictionaryRef, CFStringRef, CFRange);
+CFArrayRef DCSCopyDefinitionRecords(DCSDictionaryRef, CFStringRef, CFRange);
+CFArrayRef DCSCopyRecordsForSearchString(DCSDictionaryRef, CFStringRef,
+                                         CFRange);
+CFStringRef DCSRecordCopyData(DCSRecordRef);
+CFURLRef DCSRecordCopyDataURL(DCSRecordRef);
+CFStringRef DCSRecordCopyDefinition(DCSRecordRef);
+CFTypeRef DCSRecordCopyTextElements(DCSRecordRef);
+CFTypeRef DCSRecordGetAnchor(DCSRecordRef);
+CFTypeRef DCSRecordGetAssociatedObj(DCSRecordRef);
+CFTypeRef DCSRecordGetDictionary(DCSRecordRef);
+CFStringRef DCSRecordGetHeadword(DCSRecordRef);
+CFStringRef DCSRecordGetRawHeadword(DCSRecordRef);
+CFStringRef DCSRecordGetString(DCSRecordRef);
+CFTypeRef DCSRecordGetSubDictionary(DCSRecordRef);
+CFTypeRef DCSRecordGetSupplementalHeadword(DCSRecordRef);
+CFTypeRef DCSRecordGetTitle(DCSRecordRef);
 
 #endif /* fissure_dictionary_lost_define_h */
