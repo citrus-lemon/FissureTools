0755168aa0716b404020b2fc21cf833c97a00df4
WindProphet <lihanyuan1996@gmail.com>
2018-12-30 21:09:56 +0900

    dictionary parser start

--- Dictionary/Dictionary.rb
+++ Dictionary/Dictionary.rb
@@ -1,5 +1,35 @@
 #!/usr/bin/env ruby -w
 require_relative 'DCSDictionary/binding'
 module DictionaryServices
+  
+  module Parser
+    private
+    begin
+      require 'nokogiri'
+    rescue LoadError
+      def parse(html)
+        raise NotImplementedError, 'You need install nokogiri'
+      end
+    else
+      def parse(html)
+        html = html.gsub(/<span(\w)/) {"<span #{$1}"}
+        # Nokogiri::HTML.parse(html)
+        html
+      end
+    end
+  end
 
+  class DCSDictionary
+    def search(text, mode = 0, max_records = 100)
+      records_for_search_string(text, mode, max_records)
+    end
+  end
+
+  class DCSRecord
+    include Parser
+    def get
+      parse self.data
+    end
+  end
+  
 end
