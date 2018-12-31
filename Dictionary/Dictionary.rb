4057c8c982ad2fb207220495d58dc9ebf69eb62f
WindProphet <lihanyuan1996@gmail.com>
2018-12-31 19:41:20 +0900

    parse entry of english dictionary

--- Dictionary/Dictionary.rb
+++ Dictionary/Dictionary.rb
@@ -11,10 +11,43 @@ module DictionaryServices
         raise NotImplementedError, 'You need install nokogiri'
       end
     else
-      def parse(html)
-        html = html.gsub(/<span(\w)/) {"<span #{$1}"}
-        # Nokogiri::HTML.parse(html)
-        html
+      def _parse_head(el)
+        {
+          :text => el.css('> span.hw').children.select{|a| a.is_a? Nokogiri::XML::Text}.map(&:text).join
+        }
+      end
+      def _parse_sections(el)
+        el.css('> span.x_xd0').map do |e|
+          e.css('> .x_xdh .tg_pos').text.strip
+        end
+      end
+      def _parse_other(el)
+        {}
+      end
+      def _parse(html)
+        # html = html.gsub(/<span(\w)/) {"<span #{$1}"}
+        dic = Nokogiri::HTML.parse(html)
+        entrys = dic.xpath('/html/body/entry')
+        entrys.map do |en|
+          head = nil
+          sections = nil
+          other = []
+          en.children.each do |o|
+            if o.is_a?(Nokogiri::XML::Element) and o.name == 'span'
+              if o.classes.include? "hg"
+                raise "multi head `hg` element" if head
+                head = _parse_head(o)
+              elsif o.classes.include? "sg"
+                sections = _parse_sections(o)
+              elsif o.classes.include? "x_xo0"
+                other.push _parse_other(o)
+              end
+            else
+              puts "no span #{o}"
+            end
+          end
+          {:head => head, :sections => sections, :other => other}
+        end
       end
     end
   end
@@ -28,7 +61,7 @@ module DictionaryServices
   class DCSRecord
     include Parser
     def get
-      parse self.data
+      _parse self.data
     end
   end
   
